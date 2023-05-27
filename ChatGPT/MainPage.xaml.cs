a9c96f7d946b37a228a8d8d8c53167c0f84aff5b
manoj@home <alconsoft@gmail.com>
2023-05-27 20:32:49 +0400

    Update UI and Add BackButton Events
    Update spalsh and icon background and color chaages

--- ChatGPT/MainPage.xaml.cs
+++ ChatGPT/MainPage.xaml.cs
@@ -6,5 +6,38 @@ public partial class MainPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    protected override bool OnBackButtonPressed()
+    {
+        if(webView.CanGoBack)
+        {
+            webView.GoBack();
+            return true;
+        }
+        return base.OnBackButtonPressed();
+    }
+
+    private async void webView_Navigated(object sender, WebNavigatedEventArgs e)
+    {
+        if (e.Result == WebNavigationResult.Success)
+        {
+            webView.IsVisible = true;
+            return;
+        }
+        else
+        {
+            webView.IsVisible = false;
+            bool answer = await DisplayAlert("Something went wrong!!", "Please check your internet connection.", "Refresh", "Quit");
+
+            if (answer == true)
+            {
+                webView.Reload();
+            }
+            else
+            {
+                Application.Current.Quit();
+            }
+        }
+    }
 }
 
